92ff09288201c23bfdf8fb7d1c7635f5a123677c
Prbhat56 <21321@iiitu.ac.in>
2025-01-27 16:31:13 +0530

    cors added

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const colors =require('colors')
 const morgan =require('morgan')
 const dotenv = require('dotenv')
+const cors = require("cors");
 const connectDB = require('./config/db')
 const { application } = require('express')
 dotenv.config()
 connectDB()
 const app = express()
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+    origin:true,
+    origin:["https://doctor-appointment-client-theta.vercel.app"],
+    credentials:true
+}));
 app.use(morgan('dev'))
 app.use("/api/v1/user",require("./routes/userRoutes"))
 app.use("/api/v1/admin",require("./routes/adminRoutes"))
